84f3db0aeab60d2ae06967316394f7949090650e
Yavor Stoyanov <jstojanov2@gmail.com>
2024-04-10 17:08:14 +0300

    Small changes in css

--- src/views/layout.js
+++ src/views/layout.js
@@ -1,15 +1,11 @@
 import { html } from "../lib/lit-html.js";
 
 const guestNavTemplate = html`
-<div class="guest">
-    <a href="/login" class="button">Login</a>
-    <a href="/register" class="button">Register</a>
-</div>`;
+<a href="/login" class="button">Login</a>
+<a href="/register" class="button">Register</a>`;
 
 const userNavTemplate = html`
-<div class="user">
-    <a href="/logout" class="button">Logout</a>
-</div>`;
+<a href="/logout" class="button">Logout</a>`;
 
 export const layoutTemplate = (userData, content) => html`
 <div class="site">
@@ -18,11 +14,9 @@ export const layoutTemplate = (userData, content) => html`
             <a href="/">
                 <i class="fa-solid fa-screwdriver-wrench" style="color: #FFD43B;"></i>
             </a>
-            <div class="nav-buttons">
-                ${userData
-                ? userNavTemplate
-                : guestNavTemplate}
-            </div>
+            ${userData
+            ? userNavTemplate
+            : guestNavTemplate}
         </nav>
     </header>
 
