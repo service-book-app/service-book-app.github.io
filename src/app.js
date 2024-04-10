1d0f5768820c0aee77924f3a63d54ae71954dec1
Yavor Stoyanov <jstojanov2@gmail.com>
2024-04-10 18:17:54 +0300

    Created login view without css and functionality

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import page from './lib/page.mjs';
 import { decorateContext } from './middlewares/render.js';
 import { homeView } from './views/home.js';
+import { loginView } from './views/login.js';
 
 page(decorateContext);
 
 page('index.html', '/');
 page('/', homeView);
+page('/login', loginView);
 
 page.start();
---
+++ src/views/login.js
@@ -0,0 +1,18 @@
+import { html } from "../lib/lit-html.js";
+
+const loginTemplate = (onSubmit) => html`
+<section id="login">
+    <div class="form">
+        <h2>Login</h2>
+        <form class="login-form" @submit=${onSubmit}>
+            <input type="email" id="login-email" name="login-email" placeholder="email">
+            <input type="password" id="login-password" name="login-password"placeholder="password">
+            <button type="submit" class="button">Login</button>
+            <p>Don't have an account? <a href="/register">Create one</a></p>
+        </form>
+    <div>
+</section>`;
+
+export const loginView = (ctx) => {
+    ctx.render(loginTemplate());
+};
