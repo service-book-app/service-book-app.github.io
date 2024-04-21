b4297eae769c32bf83faa368812188c67c2f2cf7
Yavor Stoyanov <jstojanov2@gmail.com>
2024-04-21 19:41:09 +0300

    Add basic template for vehicle

--- src/views/allVehicles.js
+++ src/views/allVehicles.js
@@ -1,10 +1,18 @@
 import { html } from "../lib/lit-html.js";
 
 const vehicleTemplate = () => html`
-`;
+<div class="card">
+    <h5 class="card-title">Model</h5>
+    <p class="card-text">Short info</p>
+    <a href="#" class="button">Replacement</a>
+    <a href="#" class="button">Expenses</a>
+    <a href="#" class="button">Repair</a>
+    <a href="#" class="button">Delete</a>
+</div>`;
 
 const allVehiclesTemplate = () => html`
-<h1>All</h1>`;
+<h1>All</h1>
+${vehicleTemplate()}`;
 
 export const allVehiclesView = (ctx) => {
     ctx.render(allVehiclesTemplate());
