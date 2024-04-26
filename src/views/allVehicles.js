dd53e0e7aa3e74590730d5affe4ea82f31509ff3
Yavor Stoyanov <jstojanov2@gmail.com>
2024-04-26 08:56:06 +0300

    add css rules for card and transition to buttons

--- src/views/allVehicles.js
+++ src/views/allVehicles.js
@@ -2,16 +2,20 @@ import { html } from "../lib/lit-html.js";
 
 const vehicleTemplate = () => html`
 <div class="card">
-    <h5 class="card-title">Model</h5>
-    <p class="card-text">Short info</p>
-    <a href="#" class="button">Replacement</a>
-    <a href="#" class="button">Expenses</a>
-    <a href="#" class="button">Repair</a>
-    <a href="#" class="button">Delete</a>
+    <img src="" class="card-img-top" alt="">
+    <div class="card-body">
+        <h5 class="card-title">Model</h5>
+        <p class="card-text">Short info</p>
+        <div class="card-buttons">
+            <a href="#" class="button">Replacement</a>
+            <a href="#" class="button">Expenses</a>
+            <a href="#" class="button">Repair</a>
+            <a href="#" class="button del">Delete</a>
+        </div>
+    </div>
 </div>`;
 
 const allVehiclesTemplate = () => html`
-<h1>All</h1>
 ${vehicleTemplate()}`;
 
 export const allVehiclesView = (ctx) => {
