79cb9741ad494b3ca26874ba864604c167ad4214
Yavor Stoyanov <jstojanov2@gmail.com>
2024-07-13 19:48:06 +0300

    Changed css for navigation.

--- src/views/navigation.js
+++ src/views/navigation.js
@@ -1,22 +1,24 @@
-import { html } from "../../src/lib/lit-html.js";
+import { html, nothing } from "../../src/lib/lit-html.js";
 
 const guestNavTemplate = html`
 <a href="/login" class="button">Login</a>
 <a href="/register" class="button">Register</a>`;
 
-const userNavTemplate = (user) => html`
-<p id="welcome">Welcome ${user}</p>
+const userNavTemplate = html`
 <a href="/info" class="button">Info</a>
 <a href="/add" class="button">Add vehicle</a>
 <a href="/all" class="button">My vehicles</a>
 <a href="/logout" class="button">Logout</a>`;
 
 export const navTemplate = (userData) => html`
-<nav>
-    <a href="/">
-        <i class="fa-solid fa-screwdriver-wrench" style="color: #FFD43B;"></i>
-    </a>
-    ${userData
-    ? userNavTemplate(userData.username)
-    : guestNavTemplate}
-</nav>`;
+<div class="left-nav-container">
+    <a href="/" id="logo-holder"><i class="fa-solid fa-screwdriver-wrench" style="color: #FFD43B;"></i></a>
+    ${userData ? html`<p id="welcome">Welcome, ${userData.username}!</p>` : nothing}
+</div>
+<div class="right-nav-container">
+    <nav class="main-nav">
+        ${userData
+        ? userNavTemplate
+        : guestNavTemplate}
+    </nav>
+</div>`;
